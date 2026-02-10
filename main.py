07132ae6e85c357b144a5bb654971c1b352996d8
Rutger Berns <rutger.berns@ru.nl>
2026-02-10 11:04:51 +0100

    probable state added for benchmarking

--- main.py
+++ main.py
@@ -15,13 +15,17 @@ def main():
 
     s_random = np.random.choice([-1, 1], size=(nspins*alpha + nspins))
     s_updown = np.array([-1,1]*(all_ising_spins//2))
-    s_allup = np.ones(nspins*alpha + nspins)
+    s_allup = np.ones(nspins*alpha + nspins, dtype=int)
+    s_probable1 = np.array([-1,  1, -1,  1,  1, -1,  1, -1, -1,  1, -1,  1,  1, -1,  1, -1,  1, 1, -1, -1, -1,  1, -1,  1,  1, -1,  1, -1, -1,  1, -1,  1,  1, -1, 1, -1, -1,  1, -1,  1,  1, -1,  1, -1, -1,  1, -1,  1])
+    s_probable2 = np.array([-1,  1, -1,  1,  1, -1,  1, -1, -1,  1, -1,  1,  1, -1,  1, -1,  1,-1,  1, -1, -1,  1, -1,  1,  1, -1,  1, -1, -1,  1, -1, -1,  1, -1, 1, -1, -1,  1, -1,  1,  1, -1,  1, -1, -1,  1, -1,  1])
 
     np.set_printoptions(linewidth=400)
 
     energy_random = boltzmann_energy(W_weights, b_biases, s_random)
     energy_updown = boltzmann_energy(W_weights, b_biases, s_updown)
     energy_allup = boltzmann_energy(W_weights, b_biases, s_allup)
+    energy_probable1 = boltzmann_energy(W_weights, b_biases, s_probable1)
+    energy_probable2 = boltzmann_energy(W_weights, b_biases, s_probable2)
 
     print("========== Sample ==========")
     print("s =", s_random)
@@ -32,6 +36,14 @@ def main():
     print("========== Sample ==========")
     print("s =", s_allup)
     print("H(s) = ", energy_allup)
+    print("========== Sample ==========")
+    print("s =", s_probable1)
+    print("H(s) = ", energy_probable1)
+    print("========== Sample ==========")
+    print("s =", s_probable2)
+    print("H(s) = ", energy_probable2)
+
+
 
 if __name__ == "__main__":
     main()
